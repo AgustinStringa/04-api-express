0ecc9ca83aa9e3aa69eb9aae974c1e9c32702384
Agustín Stringa <stringaagustin1@gmail.com>
2024-04-29 13:23:57 -0300

    moving db connection to shared

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,16 @@
 import express from "express";
+import { db } from "./shared/db.js";
 import { routerApi } from "./routes/index.js";
-import { MongoClient, ServerApiVersion } from "mongodb";
+
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 
 app.get("/users", async (req, res, err) => {
-  const uri =
-    "mongodb+srv://stringaagustin1:kBdpBd9CXSeXpshJ@cluster0.asqcnur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
-  await client.connect();
-
-  const mongo = await client.db("sample_mflix").collection("users");
+  const mongo = await db.collection("users");
   const users = await mongo.find({}).limit(15).toArray();
   console.log(users);
   res.json(users);
-  await client.close();
 });
 routerApi(app);
 
